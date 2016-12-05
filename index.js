fbbadd58b2a4a6e37cf59047c93d21b1ded5a48e
Conti <agc11d@gmail.com>
2016-12-05 17:20:43 -0500

    fix(index): added callback

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const home = require('os').homedir()
 const username = require('username').sync()
-const chromeDictionary = `Library/Application Support/Google/Chrome/${home}/Custom Dictionary.txt"`
+const chromeDictionary = `Library/Application Support/Google/Chrome/${home}/Custom Dictionary.txt`
 const localDictionary  = `Users/${username}/Library/Spelling/LocalDirectory`
+const dictionaries = [chromeDictionary, localDictionary]
 
-[chromeDictionary, localDictionary].forEach(dictionary => fs.truncate(dictionary, 0))
+dictionaries.forEach(dictionary => fs.truncate(dictionary, 0, console.log))
